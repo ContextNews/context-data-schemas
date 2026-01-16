8f03bf4eb01f7cc6b08537e86b80b4d731861d9b
Euan <euan.m.campbell@protonmail.com>
2026-01-16 23:52:20 +0000

    add unique constraint to articles url

--- rds_postgres/models.py
+++ rds_postgres/models.py
@@ -14,13 +14,10 @@ class Article(Base):
     source = Column(String, nullable=False)
     title = Column(String, nullable=False)
     summary = Column(Text, nullable=False)
-    url = Column(String, nullable=False, index=True)
+    url = Column(String, nullable=False, unique=True, index=True)
     published_at = Column(DateTime, nullable=False, index=True)
     ingested_at = Column(DateTime, nullable=False, index=True)
     text = Column(Text)
-    embedded_text = Column(Text)
-    embedding = Column(Vector(None))
-    embedding_model = Column(String, nullable=False)
 
 
 class Entity(Base):
@@ -43,3 +40,12 @@ class ArticleEntity(Base):
             ["entities.type", "entities.name"],
         ),
     )
+
+
+class ArticleEmbedding(Base):
+    __tablename__ = "article_embeddings"
+
+    article_id = Column(String, ForeignKey("articles.id"), primary_key=True)
+    embedded_text = Column(Text)
+    embedding = Column(Vector(None))
+    embedding_model = Column(String, primary_key=True)
